f37a45b4284d5be89631127675ebcc4b0d3d7afb
MkHansen <Fubar93@gmail.com>
2019-04-26 12:31:41 +0200

    getMaterial method modified to test database connection.

--- src/main/java/data/DataAccessor.java
+++ src/main/java/data/DataAccessor.java
@@ -14,48 +14,49 @@ import java.util.ArrayList;
  *
  * @author Mkhansen
  */
-public class DataAccessor implements DataAcessorInterface{
-    public static void main(String[] args) {
-      
-//        System.out.println(getCupCakePrice("Jordbær"));
-//        System.out.println(getAllCupCakes().toString());
-//        System.out.println(getUser("John").toString());
-//        createUser("John", "1234", "Teeest@testmail.dk", 1000);
+public class DataAccessor implements DataAcessorInterface {
+
+//    public static void main(String[] args) {
 //
-//        System.out.println("Start");
-//        getAllCupCakes();
-//        String Jordbær = "Jordbær";
-//        CompleteCupCake j = getCupCake(Jordbær);
-//        System.out.println("CupCake found: " + j.getName());
-        System.out.println(getAllCupCakes().toString()); 
-    }
-     
-    public static ArrayList<CompleteCupCake> getAllCupCakes() {
-        ArrayList<CompleteCupCake> list = new ArrayList<>();
+////        System.out.println(getCupCakePrice("Jordbær"));
+////        System.out.println(getAllCupCakes().toString());
+////        System.out.println(getUser("John").toString());
+////        createUser("John", "1234", "Teeest@testmail.dk", 1000);
+////
+////        System.out.println("Start");
+////        getAllCupCakes();
+////        String Jordbær = "Jordbær";
+////        CompleteCupCake j = getCupCake(Jordbær);
+////        System.out.println("CupCake found: " + j.getName());
+//        System.out.println(getAllCupCakes().toString());
+//    }
 
-        try {
-            DBConnector c = new DBConnector();
-
-            String query = "SELECT * FROM CupCakes_list;";
-
-            Connection connection = c.getConnection();
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                String name = rs.getString("c_name");
-                double price = rs.getDouble("c_price");
-
-                list.add(new CompleteCupCake(name, price));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
+//    public static ArrayList<CompleteCupCake> getAllCupCakes() {
+//        ArrayList<CompleteCupCake> list = new ArrayList<>();
+//
+//        try {
+//            DBConnector c = new DBConnector();
+//
+//            String query = "SELECT * FROM CupCakes_list;";
+//
+//            Connection connection = c.getConnection();
+//            Statement stmt = connection.createStatement();
+//            ResultSet rs = stmt.executeQuery(query);
+//            while (rs.next()) {
+//                String name = rs.getString("c_name");
+//                double price = rs.getDouble("c_price");
+//
+//                list.add(new CompleteCupCake(name, price));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return list;
+//    }
 
     @Override
     public ArrayList<Material> getAllMaterials() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -63,9 +64,38 @@ public class DataAccessor implements DataAcessorInterface{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *
+     * @param name
+     * @return
+     */
     @Override
     public Material getMaterial(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Material Mat = null;
+
+        try {
+            DBConnector c = new DBConnector();
+
+            String query = "SELECT * FROM Materials WHERE MaterialName ='" + name + "';";
+
+            Connection connection = c.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String desc = rs.getString("Help_Description");
+                int length = rs.getInt("Length");
+                int height = rs.getInt("Height");
+                int width = rs.getInt("Width");
+                int priceM = rs.getInt("PriceM");
+                int priceM2 = rs.getInt("PriceM2");
+                
+                Mat = new Material(name, desc, length, height, width, priceM, priceM2);
+            }
+            return Mat;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Mat;
     }
 
     @Override
