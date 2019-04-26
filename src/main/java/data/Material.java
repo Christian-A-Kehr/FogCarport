81bd9be7c6f9e8972c66e38948d5c1885d81ad4f
MkHansen <Fubar93@gmail.com>
2019-04-26 12:30:34 +0200

    Temporary constructor + toString method created.

--- src/main/java/data/Material.java
+++ src/main/java/data/Material.java
@@ -10,5 +10,22 @@ package data;
  * @author Mkhansen
  */
 public class Material {
-    
+    public String name, desc;
+    public int length, height, width, priceM, priceM2;
+
+    public Material(String name, String desc, int length, int height, int width, int priceM, int priceM2) {
+        this.name = name;
+        this.desc = desc;
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.priceM = priceM;
+        this.priceM2 = priceM2;
+    }
+
+    @Override
+    public String toString() {
+        return "Material{" + "name=" + name + ", desc=" + desc + ", length=" + length + ", height=" + height + ", width=" + width + ", priceM=" + priceM + ", priceM2=" + priceM2 + '}';
+    }
+
 }
