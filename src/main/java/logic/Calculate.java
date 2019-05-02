0d94b9f67b2c16906178eaaffd20f47d02f35078
Claus <Claus@10.50.130.124>
2019-05-02 16:56:02 +0200

    Claus

--- src/main/java/logic/Calculate.java
+++ src/main/java/logic/Calculate.java
@@ -18,19 +18,6 @@ import data.WoodPost;
 
 public class Calculate {
     
-    private Material  shoppingcart = new Material();
-    
-    private Shed shed = new Shed();
-    private Roof roof = new Roof();
-    private Rafter rafter = new Rafter();
-    private OfferLine offerline = new OfferLine();
-    private Offer offer = new Offer();
-    private Material material = new Material();
-    private Floor floor = new Floor();
-    private DataUpdater dataupdater = new DataUpdater();
-    private Customer customer = new Customer();
-    private Carport carport = new Carport();
-    private Beam beam = new Beam();
     private DataAccessor dataaccessor = new DataAccessor();
     
     // roof. length 0,8 + 0,3 
@@ -39,7 +26,7 @@ public class Calculate {
     public double WoodPostNeeded(Carport carport) {
         // hvis tiden tillader, så skal l1, l2 og l3 kunne ændres ved hjælp af database
         
-    private WoodPost woodpost = new WoodPost(carport.getRoof().);
+    WoodPost woodpost = new WoodPost(carport.getRoof().getWoodpost());
     int l = carport.getLength();
     int l1 = 3000;
     int l2 = 800;
