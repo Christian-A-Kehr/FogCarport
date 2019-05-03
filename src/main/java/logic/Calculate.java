68b84f6c00910e850d0bcabb3402ae39e84f2d79
Claus <Claus@10.50.130.124>
2019-05-03 08:41:17 +0200

    Claus

--- src/main/java/logic/Calculate.java
+++ src/main/java/logic/Calculate.java
@@ -1,21 +1,21 @@
 package logic;
 
-import data.Beam;
-import data.Carport;
-import data.CompleteCupCake;
-import data.Customer;
-import data.DataAccessor;
-import data.DataUpdater;
-import data.Floor;
-import java.util.List;
-import data.Material;
-import data.Offer;
-import data.OfferLine;
-import data.Rafter;
-import data.Roof;
-import data.Shed;
-import data.WoodPost;
 
+import data.Carport;
+import data.DBConnector;
+import data.DataAccessor;
+import data.Material;
+import data.WoodPost;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+ /**
+ *
+ * @author Christian Ambjørn Kehr
+ * @author Claus Mikkelsen Findinge
+ */
+ 
 public class Calculate {
     
     private DataAccessor dataaccessor = new DataAccessor();
@@ -23,16 +23,47 @@ public class Calculate {
     // roof. length 0,8 + 0,3 
     
           
-    public double WoodPostNeeded(Carport carport) {
+    public int WoodPostNeeded(Carport carport) {
         // hvis tiden tillader, så skal l1, l2 og l3 kunne ændres ved hjælp af database
         
-    WoodPost woodpost = new WoodPost(carport.getRoof().getWoodpost());
-    int l = carport.getLength();
-    int l1 = 3000;
-    int l2 = 800;
-    int l3 = 300;
-    int TotalPosts = (l - (l2 + l3)) / l1;
-
-
-    } 
+//    WoodPost woodpost = new WoodPost(carport.getRoof().getWoodpost().getMaterial(),
+//                                     carport.getRoof().getWoodpost().getLenght(),
+//                                     carport.getRoof().getWoodpost().getWidth(),
+//                                     carport.getRoof().getWoodpost().getMprice());
+    // L1, L2 og L3's værdier skal laves om til variabler, som kan rettes af bruger.
+    int L = carport.getLength();
+    int L1 = 4000;
+    int L2 = 800;
+    int L3 = 300;
+    int max = (L - (L2 + L3)); 
+    int PostsPrBeams = max/L1;
     
+    // 1000 = nearest meter 
+    PostsPrBeams = (int) (Math.ceil(PostsPrBeams / 1000.0) * 1000);
+    int TotalPosts = PostsPrBeams * BeamsNeeded(carport);
+    
+    return TotalPosts;
+    }
+
+    public double WooPostTotalPrice(Carport carport) {
+        double totalPrice;
+
+        totalPrice = ((WoodPostNeeded(carport) * carport.getHeight()) * carport.getRoof().getWoodpost().getMprice());
+
+        return totalPrice;
+    }
+
+    private int BeamsNeeded(Carport carport) {
+    // B1, B2 og B3's værdier skal laves om til variabler, som kan rettes af bruger.
+    int B = carport.getLength();
+    int B1 = 4000;
+    int B2 = 150;
+    int B3 = 150;
+    int max = (B - (B2 + B3)); 
+    int PostsPrBeams = max/B1;
+    PostsPrBeams = (int) (Math.ceil(PostsPrBeams / 1000.0) * 1000);
+    int TotalPosts = PostsPrBeams * BeamsNeeded(carport);
+    
+    return TotalPosts;
+    }}
+
