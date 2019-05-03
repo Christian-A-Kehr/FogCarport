750511133880663c07a43538426580646a43e4f0
Kehr <cph-ck209@cphbusiness.dk>
2019-05-03 10:45:35 +0200

    Update to logic

---
+++ src/main/java/logic/Facade.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import data.DataAccessor;
+import data.Material;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Christian Ambjørn Kehr
+ */
+public class Facade {
+      DataAccessor data = new DataAccessor();
+     
+    public static void main(String[] args) {
+        Facade logic = new Facade();
+        //logic.getMaterialLogic();
+        logic.GetAllMatrials();
+        
+    }
+    
+    public ArrayList<Material> GetAllMatrials(){
+        ArrayList list = new ArrayList(data.getAllMaterials());
+        System.err.println(list);
+        
+        return list;
+    }
+    
+}
