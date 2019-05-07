18e47df9337606ea10dc27f42893b922155518c3
MkHansen <Bruger@10.50.130.152>
2019-05-07 12:10:43 +0200

    Class made ready to get data from logic insted of data

--- src/main/java/presentation/GoToOrderPage.java
+++ src/main/java/presentation/GoToOrderPage.java
@@ -9,6 +9,7 @@ import data.DataAccessor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import logic.Facade;
 import logic.FogException;
 
 /**
@@ -17,13 +18,24 @@ import logic.FogException;
  */
 public class GoToOrderPage extends Command {
     private DataAccessor dataAccess = new DataAccessor();
-
+    private Facade logic = new Facade();
+    
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
         HttpSession session = request.getSession();
         session.setAttribute("allRoofMats", dataAccess.GetListSpecificMaterials("Tagsten"));
-        
+        session.setAttribute("allRafterMats", dataAccess.GetListSpecificMaterials("Spær"));
+        session.setAttribute("allShedMats", dataAccess.GetListSpecificMaterials("Beklædning"));
+        session.setAttribute("allFloorMats", dataAccess.GetListSpecificMaterials("Gulv"));
+        session.setAttribute("allWoodpostMats", dataAccess.GetListSpecificMaterials("Stolpe"));
+        session.setAttribute("allBeamMats", dataAccess.GetListSpecificMaterials("Rem"));
+
+//        session.setAttribute("allRoofMats", logic.GetListSpecificMaterials("Tagsten"));
+//        session.setAttribute("allRafterMats", logic.GetListSpecificMaterials("Spær"));
+//        session.setAttribute("allShedMats", logic.GetListSpecificMaterials("Beklædning"));
+//        session.setAttribute("allFloorMats", logic.GetListSpecificMaterials("Gulv"));
+//        session.setAttribute("allWoodpostMats", logic.GetListSpecificMaterials("Stolpe"));
+//        session.setAttribute("allBeams", logic.GetListSpecificMaterials("Rem"));
         return "orderPage";
     }
-
 }
