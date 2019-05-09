9f4a3bbebaa78ca5d4f96119d7170a0aaabe3ea1
MkHansen <Bruger@10.50.130.152>
2019-05-09 15:13:05 +0200

    Fixed data flow

--- src/main/java/presentation/GoToOrderPage.java
+++ src/main/java/presentation/GoToOrderPage.java
@@ -5,7 +5,8 @@
  */
 package presentation;
 
-import data.DataAccessor;
+import data.Material;
+import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,17 +19,19 @@ import logic.FogException;
  */
 public class GoToOrderPage extends Command {
     private Facade logic = new Facade();
+    private ArrayList<Material> chosenMats = new ArrayList<>();
     
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
         HttpSession session = request.getSession();
-
+        
         session.setAttribute("allRoofMats", logic.getListSpecificMaterials("Tagsten"));
         session.setAttribute("allRafterMats", logic.getListSpecificMaterials("Spær"));
         session.setAttribute("allShedMats", logic.getListSpecificMaterials("Beklædning"));
         session.setAttribute("allFloorMats", logic.getListSpecificMaterials("Gulv"));
         session.setAttribute("allWoodpostMats", logic.getListSpecificMaterials("Stolpe"));
         session.setAttribute("allBeamMats", logic.getListSpecificMaterials("Rem"));
+        session.setAttribute("chosenMats", chosenMats);
         return "orderPage";
     }
 }
