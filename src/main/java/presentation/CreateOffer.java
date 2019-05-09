5ddd2bd3a03c59e181d5c8e09789314011257421
MkHansen <Bruger@10.50.130.152>
2019-05-09 15:13:25 +0200

    trying to fix data flow to the new jsp

--- src/main/java/presentation/CreateOffer.java
+++ src/main/java/presentation/CreateOffer.java
@@ -5,37 +5,48 @@
  */
 package presentation;
 
-import java.util.ArrayList;
+import data.Beam;
+import data.Carport;
+import data.Floor;
+import data.Rafter;
+import data.Roof;
+import data.Shed;
+import data.WallCoverings;
+import data.WoodPost;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import logic.Facade;
 import logic.FogException;
-
 
 /**
  *
  * @author Mkhansen
  */
-public class CreateOffer extends Command{
+public class CreateOffer extends Command {
+
+    private Facade logic = new Facade();
+
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
         HttpSession session = request.getSession();
-        session.setAttribute("chosenMats", new ArrayList<>());
-        session.getAttribute("chosenRoofMats");
+//        session.setAttribute("chosenMats", ArrayList<>());
+//        request.getParameter("chosenMats");
+
+//        WallCoverings wallCoverings = new WallCoverings(request.getParameter() );
+//        Floor floor = new Floor();
+//        Shed shed = new Shed(request.getParameter("shedDepth"),request.getParameter("shedWidth"),wallCoverings ,floor);
+//        Beam beam = new Beam(request.getParameter("beam"));
+//        Rafter rafter = new Rafter();
+//        WoodPost woodPost = new WoodPost();
+//        Roof roof = new Roof();
+//        Carport carport = new Carport();
         
-        
-        
-        
-        
-        
-        session.setAttribute("allRoofMats", logic.getListSpecificMaterials("Tagsten"));
-        session.setAttribute("allRafterMats", logic.getListSpecificMaterials("Spær"));
-        session.setAttribute("allShedMats", logic.getListSpecificMaterials("Beklædning"));
-        session.setAttribute("allFloorMats", logic.getListSpecificMaterials("Gulv"));
-        session.setAttribute("allWoodpostMats", logic.getListSpecificMaterials("Stolpe"));
-        session.setAttribute("allBeamMats", logic.getListSpecificMaterials("Rem"));
-        
-        return "";
+        System.out.println(request.getParameter("flatOrNotChoice"));
+        System.out.println(request.getParameter("chosenMats"));
+        session.getAttribute("chosenMats");
+        session.setAttribute("chosenMats", request.getParameter("chosenMats"));
+
+        return "test";
     }
-    
 }
