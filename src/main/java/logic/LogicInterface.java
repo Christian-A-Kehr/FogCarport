97f25680760f9976d69e05a7acfe08ddd9b453e9
Claus <Claus@10.50.130.124>
2019-05-10 13:49:16 +0200

    Claus

--- src/main/java/logic/LogicInterface.java
+++ src/main/java/logic/LogicInterface.java
@@ -24,9 +24,9 @@ public interface LogicInterface {
     public int calulateGabledHeight(Carport carport);
     public int calculateGabledArea(Carport carport);
     public int calculateGabledWallCovering(Roof roof);
-    public int totalWallCoveringsNeeded(Shed shed);
-    public double calculateWallCoveringPrice(int area, Carport carport);
     public int WallCoveringsNeededDepth(Shed shed);
+    public double calculateWallCoveringPrice(int area, Carport carport);
+    public int totalWallCoveringsNeeded(Shed shed);
     public int WallCoveringsNeededwidth(Shed shed);
     public int roofArea(Carport carport); 
    // public carport GetPrices(Carport carport);
