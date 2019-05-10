f2befb40e0a458282a6d0a73d28073403fb44edc
MkHansen <Bruger@10.50.130.152>
2019-05-10 13:47:19 +0200

    objects fixed

--- src/main/java/presentation/CreateOffer.java
+++ src/main/java/presentation/CreateOffer.java
@@ -30,17 +30,23 @@ public class CreateOffer extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
         HttpSession session = request.getSession();
+            int height = Integer.parseInt(request.getParameter("height"));
+            int length = Integer.parseInt(request.getParameter("height"));
+            int width = Integer.parseInt(request.getParameter("height"));
+            int shedWidth = Integer.parseInt(request.getParameter("shedDepth"));
+            int shedDepth = Integer.parseInt(request.getParameter("shedWidth"));
+            
 
-        WallCoverings wallCoverings = new WallCoverings(request.getParameter("shedChoice"), Integer.parseInt(request.getParameter("height")), Integer.parseInt(request.getParameter("shedDepth")), Integer.parseInt(request.getParameter("shedWidth")));
-        Floor floor = new Floor(request.getParameter("floorChoice"), Integer.parseInt(request.getParameter("shedDepth")), Integer.parseInt(request.getParameter("shedLength")));
-        Shed shed = new Shed(Integer.parseInt(request.getParameter("shedDepth")), Integer.parseInt(request.getParameter("shedWidth")),wallCoverings ,floor);
-        Beam beam = new Beam(request.getParameter("beamChoice"), Integer.parseInt(request.getParameter("length")));
-        Rafter rafter = new Rafter(request.getParameter("rafterChoice"), Integer.parseInt(request.getParameter("width")) );
-        WoodPost woodpost = new WoodPost(request.getParameter("woodpostChoice"), Integer.parseInt(request.getParameter("height")));
-        Roof roof = new Roof( request.getParameter("flatOrNot"), request.getParameter("roofChoice"), Integer.parseInt(request.getParameter("angleChoice")), Integer.parseInt(request.getParameter("length")), Integer.parseInt(request.getParameter("width")), beam, rafter, woodpost);
-        Carport carport = new Carport( Integer.parseInt(request.getParameter("height")), Integer.parseInt(request.getParameter("length")), Integer.parseInt(request.getParameter("width")), roof, shed);
-        
-        
+            WallCoverings wallCoverings = new WallCoverings(request.getParameter("shedChoice"),height , shedDepth, shedWidth);
+            Floor floor = new Floor(request.getParameter("floorChoice"), shedDepth, shedWidth);
+            Shed shed = new Shed(shedDepth, shedWidth, wallCoverings, floor);
+            
+            Beam beam = new Beam(request.getParameter("beamChoice"), Integer.parseInt(request.getParameter("length")));
+            Rafter rafter = new Rafter(request.getParameter("rafterChoice"), Integer.parseInt(request.getParameter("width")));
+            WoodPost woodpost = new WoodPost(request.getParameter("woodpostChoice"), Integer.parseInt(request.getParameter("height")));
+            Roof roof = new Roof(request.getParameter("flatOrNot"), request.getParameter("roofChoice"), Integer.parseInt(request.getParameter("angleChoice")), Integer.parseInt(request.getParameter("length")), Integer.parseInt(request.getParameter("width")), beam, rafter, woodpost);
+            Carport carport = new Carport(Integer.parseInt(request.getParameter("height")), Integer.parseInt(request.getParameter("length")), Integer.parseInt(request.getParameter("width")), roof, shed);
+
         return "test";
     }
 }
