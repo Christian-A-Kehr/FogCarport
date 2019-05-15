9d1d9a662bb216a4e2104c6f5dc5a27312f3a182
Claus <Claus@10.50.130.124>
2019-05-15 15:10:54 +0200

    added command

---
+++ src/main/java/presentation/CommandDrawing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import logic.FogException;
+
+/**
+ *
+ * @author Slantefar
+ */
+public class CommandDrawing extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
+//        Rafter rafter = new Rafter(36.0, 44.0, 33.0);
+//        Calculate calculate = new Calculate();
+//        calculate.BeamsNeeded();
+//        request.setAttribute("rafter", rafter);
+//        request.getRequestDispatcher("Draw.jsp").forward(request, response);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
