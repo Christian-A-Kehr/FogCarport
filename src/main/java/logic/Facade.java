99a92f647ba2ada2600071f91f3fd715181fb6ca
Kehr <cph-ck209@cphbusiness.dk>
2019-05-15 13:10:18 +0200

    Logic A

--- src/main/java/logic/Facade.java
+++ src/main/java/logic/Facade.java
@@ -6,39 +6,86 @@
 package logic;
 
 import data.DataAccessor;
+import data.DataUpdater;
+import data.Delivery;
+import data.Demand;
 import data.Material;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Christian Ambjørn Kehr
  */
 public class Facade {
-      DataAccessor data = new DataAccessor();
-     
+
+    DataAccessor data = new DataAccessor();
+
     public static void main(String[] args) {
         Facade logic = new Facade();
         //logic.getMaterialLogic();
 //        logic.getAllMaterials();
-            System.out.println(logic.getMaterial("Spær C18"));
-        
+        System.out.println(logic.getMaterial("Spær C18"));
+
     }
-    
-    public ArrayList<Material> getAllMaterials(){
+///////////////////////////////////dataAccessor///////////////////////////////////
+
+    public ArrayList<Material> getAllMaterials() {
         ArrayList list = new ArrayList(data.getAllMaterials());
         System.err.println(list);
-        
+
         return list;
     }
-    public ArrayList<Material> getListSpecificMaterials(String type){
+
+    public ArrayList<Material> getListSpecificMaterials(String type) {
         return data.getListSpecificMaterials(type);
     }
-    
-    public Material getMaterial(String name){
+
+    public Material getMaterial(String name) {
         return data.getMaterial(name);
     }
-    public Material getMaterialFromId(int id){
+
+    public Material getMaterialFromId(int id) {
         return data.getMaterialFromId(id);
     }
+
+    /////////////////////////////////DataUpdater///////////////////////////////////////////////////
     
+    DataUpdater dataUp = new data.DataUpdater();
+
+    public void saveMaterial(int id, Material material) {
+        List<Material> AllMats = dataUp.DisplayAllMaterial();
+
+        for (int i = 0; i < AllMats.size(); i++) {
+            if (AllMats.get(i).getMatNum() == id) {
+                dataUp.updateMaterial(id, material);
+            }
+            dataUp.createMaterial(material);
+        }
+    }
+
+    public void dropMaterial(int id) {
+        dataUp.dropMaterial(id);
+    }
+
+    public List<Material> DisplayAllMaterial() {
+        return DisplayAllMaterial();
+    }
+
+    public Delivery getDelivery(String location) {
+        return dataUp.getDelivery(location);
+    }
+
+    public void updateDeliveryPrice(String location, double price) {
+        dataUp.updateDeliveryPrice(location, price);
+    }
+
+    public Demand getVariable(String name) {
+        return dataUp.getVariable(name);
+    }
+
+    public void updateDemandVariables(String name, int measurements) {
+        dataUp.updateDemandVariables(name, measurements);
+    }
+
 }
--- src/main/java/logic/Logicfacade.java
+++ src/main/java/logic/Logicfacade.java
@@ -5,10 +5,55 @@
  */
 package logic;
 
+import data.DataUpdater;
+import data.Delivery;
+import data.Demand;
+import data.Material;
+import java.util.List;
+
 /**
  *
  * @author Christian Ambjørn Kehr
  */
 public class Logicfacade {
+DataUpdater dataUp = new data.DataUpdater();
+
+
+    public void saveMaterial(int id ,Material material){
+        List<Material> AllMats= dataUp.DisplayAllMaterial();
+        
+            for (int i = 0; i < AllMats.size(); i++) {
+            if (AllMats.get(i).getMatNum() == id){
+               dataUp.updateMaterial(id, material);
+            }
+            dataUp.createMaterial(material);
+            }
+    }
+
+    public void dropMaterial(int id) {
+        dataUp.dropMaterial(id);
+    }
+
+    public List<Material> DisplayAllMaterial() {
+        return DisplayAllMaterial();
+    }
+
+    public Delivery getDelivery(String location) {
+        return dataUp.getDelivery(location);
+    }
+
+    public void updateDeliveryPrice(String location, double price) {
+        dataUp.updateDeliveryPrice(location, price);
+    }
+
+    public Demand getVariable(String name) {
+        return dataUp.getVariable(name);
+    }
+
+    public void updateDemandVariables(String name, int measurements) {
+        dataUp.updateDemandVariables(name, measurements);
+    }
     
+
+
 }
