53063d83f6c4b3c8d121f7e77722ddd7c5ec246a
MkHansen <Bruger@10.50.130.152>
2019-05-21 14:09:31 +0200

    ChangeDelivery class created

---
+++ src/main/java/presentation/ChangeDelivery.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import logic.Facade;
+import logic.FogException;
+
+/**
+ *
+ * @author Mkhansen
+ */
+public class ChangeDelivery extends Command {
+
+    private Facade logic = new Facade();
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws FogException {
+        response.setContentType("text/html,charset=UTF8");
+        logic.updateDeliveryPrice(request.getParameter("delivery"), Double.parseDouble(request.getParameter("newPrice")));
+        return new Settings().execute(request, response);
+    }
+
+}
